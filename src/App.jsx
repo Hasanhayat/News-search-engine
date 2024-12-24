ad2ee675824325550dd9fde1429580f0e364b6d9
hasanhayat <aylskhsmvz.v@gmail.com>
2024-12-24 21:08:37 +0500

    mid

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import axios from "axios";
 import { Card } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
 
 function App() {
   const [news, setNews] = useState([]);
@@ -69,20 +70,32 @@ function App() {
       </label>
 
       <div className="d-flex flex-column align-items-center gap-3">
-        {news.map((e, i) => {
-          return (
-            <Card key={i} style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={e?.image_url} />
-              <Card.Body>
-                <Card.Title>{e?.title}</Card.Title>
-                <Card.Text>{e?.description}</Card.Text>
-              </Card.Body>
-              <Card.Body>
-                <Card.Link href={e?.source_url}>{e?.source_name}</Card.Link>
-              </Card.Body>
-            </Card>
-          );
-        })}
+        <Carousel>
+          {news.map((e, i) => {
+            return (
+              <Carousel.Item key={i} interval={1000}>
+                <img src={e?.img_url} alt="" />
+                <Carousel.Caption>
+                  <h3>{e?.title}</h3>
+                  <p>
+                    {e?.description}
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+
+              // <Card key={i} style={{ width: "18rem" }}>
+              //   <Card.Img variant="top" src={e?.image_url} />
+              //   <Card.Body>
+              //     <Card.Title>{e?.title}</Card.Title>
+              //     <Card.Text>{e?.description}</Card.Text>
+              //   </Card.Body>
+              //   <Card.Body>
+              //     <Card.Link href={e?.source_url}>{e?.source_name}</Card.Link>
+              //   </Card.Body>
+              // </Card>
+            );
+          })}
+        </Carousel>
       </div>
     </div>
   );
